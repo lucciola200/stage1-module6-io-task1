715645d2b3223dcfecf87ee17cb89c73eb4f6fa2
lucciolla200 <teoteo.odisharia@gmail.com>
2022-08-22 10:02:53 +0400

    Task IO - done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,52 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.Locale;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        InputStream inputStream = null;
+        String fileTxt = "";
+        Profile profile = new Profile();
+
+        try {
+            inputStream = new FileInputStream(file);
+
+            for (int ch; (ch = inputStream.read()) != -1; ) {
+                fileTxt += ((char) ch);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        String[] arrfile = fileTxt.split(System.lineSeparator());
+        for (int i = 0; i < arrfile.length; i++) {
+            String keyVal = arrfile[i];
+            String[] parts = keyVal.split(": ", 2);
+            String key = parts[0].toLowerCase(Locale.ROOT);
+            if (key.equals("name")) {
+                profile.setName(parts[1]);
+            } else if (key.equals("age")) {
+                profile.setAge(Integer.parseInt(parts[1]));
+            } else if (key.equals("email")) {
+                profile.setEmail(parts[1]);
+            } else if (key.equals("phone")) {
+                profile.setPhone(Long.parseLong(parts[1]));
+            }
+        }
+        return profile;
     }
 }
